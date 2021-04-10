1f7282d3ba60795ce3a7c8a1f4d705de8cb2092b
Su <1535631290@qq.com>
2021-04-10 17:04:08 +0800

    可以判断请求源的JSONP

--- qq-com/server.js
+++ qq-com/server.js
@@ -40,6 +40,20 @@ var server = http.createServer(function (request, response) {
     response.setHeader("Access-Control-Allow-Origin", "http://frank.com:9990");
     response.write(fs.readFileSync("./public/friends.json"));
     response.end();
+  } else if (path === "/friends.js") {
+      if(request.headers['referer'].indexOf("http://frank.com:9990")===0){
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/javascript;charset=utf-8");
+        const string = fs.readFileSync("./public/friends.js").toString();
+        const data = fs.readFileSync("./public/friends.json").toString();
+        const string2 = string.replace("{{data}}", data);
+        response.write(string2);
+        response.end();
+      }else{
+          response.statusCode=404
+          response.end()
+      }
+    
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
